02698e8e1351e22cf7bacab0da765c2c7fa95fdf
torche <agung.pramono@torcheindonesia.com>
2025-01-28 18:20:37 +0700

    renewal token

--- abc.py
+++ abc.py
@@ -7,7 +7,13 @@ load_dotenv()
 app_secret = os.getenv('APP_SECRET')
 fernet = Fernet(app_secret)
 data =  {"token":"763373424:9kptk7oa", "request":"boyneh@ymail.com"}
-token = 'gAAAAABnPJw5B5RlAH3ym7MmO7pJpTmkOOoUtwPuwD3Wd8PN1N7x-oNeFuHfUrD2MP8VfCAKGh7bjrRJw26k5uAKPZIMMzVkPo1GPo4Tjy8pWWzqw3xjC7Y='
-new_token = fernet.decrypt(token.encode()).decode()
-response = requests.post(new_token, json=data)
-datajson= response.json()
+# token = 'gAAAAABnPJw5B5RlAH3ym7MmO7pJpTmkOOoUtwPuwD3Wd8PN1N7x-oNeFuHfUrD2MP8VfCAKGh7bjrRJw26k5uAKPZIMMzVkPo1GPo4Tjy8pWWzqw3xjC7Y='
+token='763373424:EXnSh7sW'
+# new_token = fernet.decrypt(token.encode()).decode()
+##fernet encrypt token
+new_token = fernet.encrypt(token.encode()).decode()
+print(new_token)
+new_token = fernet.decrypt(new_token.encode()).decode()
+print(new_token)
+# response = requests.post(new_token, json=data)
+# datajson= response.json()
--- breach1.py
+++ breach1.py
@@ -6,7 +6,7 @@ from trait import ResponseSuccess
 
 #used here
 def find_data(q):
-    data =  {"token":"763373424:9kptk7oa", "request":q, "limit": 100, "lang":"en"}
+    data =  {"token":"763373424:EXnSh7sW", "request":q, "limit": 100, "lang":"en"}
     token = 'gAAAAABnPJw5B5RlAH3ym7MmO7pJpTmkOOoUtwPuwD3Wd8PN1N7x-oNeFuHfUrD2MP8VfCAKGh7bjrRJw26k5uAKPZIMMzVkPo1GPo4Tjy8pWWzqw3xjC7Y='
     new_token = fernet.decrypt(token.encode()).decode()
     response = token1(new_token, json=data)
